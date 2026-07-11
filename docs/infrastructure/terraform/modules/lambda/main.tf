7cd47973c238e9aee169ed0d4b1a49192641234f
Neha sharma <nehasharma9d16@gmail.com>
2026-07-11 19:32:27 +0530

    Fix Lambda IAM architecture and update Terraform configuration

--- docs/infrastructure/terraform/modules/lambda/main.tf
+++ docs/infrastructure/terraform/modules/lambda/main.tf
@@ -1,78 +1,49 @@
-resource "aws_iam_role" "lambda_role" {
+data "archive_file" "lambda_zip" {
 
-  name = "${var.project_name}-${var.environment}-lambda-role"
+  type = "zip"
 
-  assume_role_policy = jsonencode({
+  source_dir = "${path.module}/src"
 
-    Version = "2012-10-17"
-
-    Statement = [
-
-      {
-
-        Action = "sts:AssumeRole"
-
-        Effect = "Allow"
-
-        Principal = {
-
-          Service = "lambda.amazonaws.com"
-
-        }
-
-      }
-
-    ]
-
-  })
+  output_path = "${path.module}/lambda.zip"
 
 }
 
-resource "aws_iam_role_policy_attachment" "basic" {
-
-  role = aws_iam_role.lambda_role.name
-
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-
-}
-
-resource "aws_iam_role_policy_attachment" "vpc" {
-
-  role = aws_iam_role.lambda_role.name
-
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
-
-}
 
 resource "aws_lambda_function" "backend" {
 
   function_name = "${var.project_name}-${var.environment}-backend"
 
+
   role = var.lambda_role_arn
+
 
   runtime = "nodejs20.x"
 
+
   handler = "index.handler"
+
 
   filename = data.archive_file.lambda_zip.output_path
 
+
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
 
   timeout = 30
 
+
   memory_size = 512
 
+
   vpc_config {
+
     subnet_ids = var.private_subnet_ids
+
 
     security_group_ids = [
       var.lambda_security_group_id
     ]
-  }
-}
 
-data "archive_file" "lambda_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/src"
-  output_path = "${path.module}/lambda.zip"
+  }
+
 }
--- docs/infrastructure/terraform/modules/lambda/src/index.js
+++ docs/infrastructure/terraform/modules/lambda/src/index.js
@@ -1,11 +1,10 @@
 exports.handler = async (event) => {
 
-    console.log("CareerGenie Lambda triggered");
-
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: "Hello from CareerGenie Lambda"
+            message: "CareerGenie Lambda running"
         })
     };
+
 };
